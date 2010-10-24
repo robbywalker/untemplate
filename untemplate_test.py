8e19fc4d0d3673ea3a862be2478e9a65fdf150ba
Robby Walker <robby.walker@gmail.com>
2010-10-24 11:56:52 -0700

    More tests

--- untemplate_test.py
+++ untemplate_test.py
@@ -12,10 +12,33 @@ class GDataUtilityTest(unittest.TestCase):
   """Test case for GData utility methods."""
 
   def testGetData(self):
+    # Test the basic case.
     self.assertEquals(
-      ('World', 'Earth'),
+      [['World'], ['Earth']],
       untemplate.getData(
         'Hello World!',
         'Hello Earth!'
       )
     )
+
+    # Test a case where the template defined by the first two documents isn't generic enough.
+    self.assertEquals(
+      [['Mary Kate'], ['Mary Jane'], ['Bob']],
+      untemplate.getData(
+        'Hello Mary Kate.  How are you?',
+        'Hello Mary Jane.  How are you?',
+        'Hello Bob.  How are you?'
+      )
+    )
+
+    # No matter which two documents we try first, the template will not be generic enough.  Also tests
+    # multiple simultaneous return values.
+    self.assertEquals(
+      [['1', '2', '3'], ['2', '2', '4'], ['1', '7', '8']],
+      untemplate.getData(
+        '1 + 2 = 3',
+        '2 + 2 = 4',
+        '1 + 7 = 8'
+      )
+    )
+
